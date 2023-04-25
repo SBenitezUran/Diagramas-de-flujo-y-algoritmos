009a57131809e9457a79929abf7a0f800a3a1e1c
unknown <santiagobenitez467@gmail.com>
2023-04-25 18:02:33 -0500

    bootcam

---
+++ WorkBookcondicionales.py
@@ -0,0 +1,13 @@
+# Para determinar si un número es negativo o positivo.
+num = float(input("introduce un número:"))
+if num > 0:
+  print("El número es positivo.")
+else:
+  print("El número es negativo.")
+
+# mayor o menor de edad.
+num = float(input("introduzca su edad:"))
+if num >= 18:
+    print("Es mayor de edad.")
+else:
+    print("Es menor de edad.")
